c295b6b3ddbb62b4619bdcb4621136cfc74e425a
Cesc Zafra <fzafra@vlex.com>
2020-03-15 19:43:42 +0100

    master: adding tests

--- spec/eltiempo_spec.rb
+++ spec/eltiempo_spec.rb
@@ -1,9 +1,39 @@
 RSpec.describe Eltiempo do
-  it "has a version number" do
-    expect(Eltiempo::VERSION).not_to be nil
-  end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+	describe "'Temperature' class" do
+		it "a new class could be created" do
+			expect(Eltiempo::Temperature.new).not_to be nil
+		end
+
+		it "needs city and the needed forecast" do
+			expect(Eltiempo::Temperature.new.today("")).to be nil
+		end
+
+		it "returns the temperature for today" do
+			expect(Eltiempo::Temperature.new.today(1200)).not_to be nil
+		end
+
+		it "returns the minimum average temperature" do
+			expect(Eltiempo::Temperature.new.av_min(1200)).not_to be nil
+		end
+
+		it "returns the maximum average temperature" do
+			expect(Eltiempo::Temperature.new.av_max(1200)).not_to be nil
+		end
+
+	end
+
+	describe "'City' class" do
+		it "a new class could be created" do
+			expect(Eltiempo::City.new).not_to be nil
+		end
+
+		it "a exact city returns the city_id" do
+			expect(Eltiempo::City.new.getCityId("Barcelona")).to eq(1200)
+		end
+
+		it "a not-found city returns city suggestions" do
+			expect(Eltiempo::City.new.getCitySuggestions("arenys")).to eq(["arenys de mar", "arenys de munt"])
+		end
+	end
 end
